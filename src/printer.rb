6a12c605b57cf7a11b57c503c1f106048f833d88
Joshua Tompkins <jtompkins@newrelic.com>
2017-02-19 21:42:06 -0800

    Pipeline operator (sort of)

---
+++ src/printer.rb
@@ -0,0 +1,15 @@
+class Printer
+  def call(cells)
+    x_range = Range.new(*cells.minmax_by(&:x).map(&:x))
+    y_range = Range.new(*cells.minmax_by(&:y).map(&:y))
+
+    puts "X: #{x_range} Y: #{y_range}"
+
+    x_range.each do |x|
+      y_range.each do |y|
+        print cells.include?(LiveCell.new(x, y)) ? 'X' : '.'
+      end
+      puts
+    end
+  end
+end
